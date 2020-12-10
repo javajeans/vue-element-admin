0b4e0a88ec8b2468a233d5e50bf9b3cab4ceda04
lizhiheng <admin@idinu.com>
2020-12-10 17:27:41 +0800

    edit ModuleFederationPlugin config

--- vue.config.js
+++ vue.config.js
@@ -52,9 +52,14 @@ module.exports = {
     plugins: [
       new VueLoaderPlugin(),
       new ModuleFederationPlugin({
-        name: "workflow",
-        remotes: {
-          app1: "workflow@http://localhost:9529/remoteEntry.js",
+        // 提供给其他服务加载的文件
+        filename: "remoteEntry.js",
+        // 唯一ID，用于标记当前服务
+        name: "app1",
+        library: { type: "var", name: "app1" },
+        // 需要暴露的模块，使用时通过 `${name}/${expose}` 引入
+        exposes: {
+            './Header': "./src/components/Header.vue",
         }
       })
     ]
